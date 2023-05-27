f5a3aa35a30ec847467b9f93eedd44db5b19d0cf
JhovaniVC <velazquezchialex@gmail.com>
2023-05-26 19:59:22 -0500

    noticia
    Victor eres zorra

---
+++ noticias/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:noticias/src/pages/tabs_page.dart';
+import 'package:noticias/src/services/news_service.dart';
+import 'package:noticias/src/theme/tema.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => NewsService()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        theme: miTema,
+        home: TabsPage(),
+      ),
+    );
+  }
+}
