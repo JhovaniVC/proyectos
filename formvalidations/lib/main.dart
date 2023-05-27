407f1cf2b2373e2479617c60126b5c9ec62112b2
JhovaniVC <velazquezchialex@gmail.com>
2023-05-26 20:02:47 -0500

    Sandro chupame un huevo

---
+++ formvalidations/lib/main.dart
@@ -0,0 +1,47 @@
+//@dart=2.9
+import 'package:flutter/material.dart';
+import 'package:formvalidations/src/bloc/provider.dart';
+import 'package:formvalidations/src/pages/home_page.dart';
+import 'package:formvalidations/src/pages/login_page.dart';
+import 'package:formvalidations/src/pages/producto_page.dart';
+import 'package:formvalidations/src/pages/registro_page.dart';
+import 'package:formvalidations/src/preferencias_usuario/preferencias_usuario.dart';
+
+void main() async { 
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final prefs = new PreferenciasUsuario();
+    await prefs.initPrefs();
+  
+  runApp(MyApp()); 
+  // https://www.facebook.com/groups/924055665581563/
+  }
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    final prefs = new PreferenciasUsuario();
+    print(prefs.token);
+    
+    return Provider(
+      
+      key: UniqueKey(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        initialRoute: 'login',
+        routes: {
+          'login' : ( BuildContext context ) => LoginPage(),
+          'registro' : ( BuildContext context ) => RegistroPage(),
+          'home'  : ( BuildContext context ) => HomePage(),
+          'producto'  : ( BuildContext context ) => ProductoPage(),
+        },
+        theme: ThemeData(
+          primaryColor: Colors.deepPurple
+        ),
+      )
+    );
+  }
+}
